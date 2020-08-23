c6043bf7ee7b99ab8fe59296d5f093db5cb587e0
Hoang Do <do000075@algonquinlive.com>
2020-08-23 15:32:49 -0400

    Updates

--- effect.js
+++ effect.js
@@ -1,24 +1,73 @@
 // NOW I CLICK album-poster TO GET CURRENT SONG ID
+var currentDataSwitch = -1;
+const play = document.querySelector(".play");
+const pause = document.querySelector(".pause");
+
 $(".album-poster").on("click", function (e) {
   var dataSwitchId = $(this).attr("data-switch");
   //console.log(dataSwitchId);
 
   // and now i use aplayer switch function see
-  ap.list.switch(dataSwitchId); //this is static id but i use dynamic
+  if (currentDataSwitch != dataSwitchId) {
+    //remove old pause and play
+    currentDataSwitch = dataSwitchId;
+    $(document).ready(function () {
+      $(".pause").remove();
+      $(".play").remove();
+    });
+
+    $(document).ready(function () {
+      $(".app" + dataSwitchId).append(' <div class="pause "></div> <div class="play active"></div>');
+    });
+
+    //this is static id but i use dynamic
+
+    //create pause
+
+    ap.list.switch(dataSwitchId);
+    $(document).ready(function () {
+      $(".pause").append("<div class=\"line line_1\"></div> <div class=\"line line_2\"></div>");
+      $(".play").append("<div class=\"line line_1\"></div> <div class=\"line line_2\"></div> <div class=\"line line_3\"></div>");
+    });
+
+    // pause.classList.remove("active");
+    // play.classList.add("active");
+    // ap.play();
+
+  } else {
+    //change play
+    if (play.classList.contains('active')) {
+      play.classList.remove('active');
+      pause.classList.add('active');
+      //add active for pause
+      ap.pause();
+    } else {
+      pause.classList.remove('active');
+      play.classList.add('active');
+      ap.play();
+    }
+  }
+  //remove play line
+
+  //add pause line
 
   // aplayer play function
   // when i click any song to play
-  ap.play();
+
+  //should have condition for turn on play and pause
 
   // click to slideUp player see
+  ap.play();
   $("#aplayer").addClass("showPlayer");
 });
+
+
+
 
 const ap = new APlayer({
   container: document.getElementById("aplayer"),
   listFolded: true,
-  audio: [
-    {
+  audio: [{
       name: "Believer",
       artist: "Imagine Dragon",
       url: "./music/Believer.mp3",
@@ -68,3 +117,31 @@ const ap = new APlayer({
     },
   ],
 });
+
+var dataSwitchId = $(this).attr("data-switch");
+if (currentDataSwitch != dataSwitchId) {
+  autoSwitchPlay();
+}
+
+function autoSwitchPlay() {
+  currentDataSwitch = dataSwitchId;
+  $(document).ready(function () {
+    $(".pause").remove();
+    $(".play").remove();
+  });
+
+  $(document).ready(function () {
+    $(".app" + dataSwitchId).append(' <div class="pause "></div> <div class="play active"></div>');
+  });
+
+  //this is static id but i use dynamic
+
+  //create pause
+
+  ap.list.switch(dataSwitchId);
+  $(document).ready(function () {
+    $(".pause").append("<div class=\"line line_1\"></div> <div class=\"line line_2\"></div>");
+    $(".play").append("<div class=\"line line_1\"></div> <div class=\"line line_2\"></div> <div class=\"line line_3\"></div>");
+  });
+
+}
